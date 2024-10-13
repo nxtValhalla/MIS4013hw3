ea5b45af8a9eaa8c1222dd44b9fd9c78ccf0ac6e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-13 15:26:37 -0500

    Create view-nbateams.php

---
+++ view-nbateams.php
@@ -0,0 +1,29 @@
+<h1>NBA Teams</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Team Name</th>
+      <th>Wins</th>
+      <th>Losses</th>
+      <th>Location ID</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($nbateam = $nbateams->fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $nbateam['TeamID']; ?></td>
+    <td><?php echo $nbateam['TeamName']; ?></td>
+    <td><?php echo $nbateam['Wins']; ?></td>
+    <td><?php echo $nbateam['Losses']; ?></td>
+    <td><?php echo $nbateam['LocationID']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
