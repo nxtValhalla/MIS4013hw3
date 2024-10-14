3198cda8c3c0ebd57b77bb35d78cfc84414485e2
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-13 19:40:34 -0500

    Update roster-by-team.php

--- roster-by-team.php
+++ roster-by-team.php
@@ -4,7 +4,7 @@ require_once("model-roster-by-team.php");
 
 $pageTitle = "Roster by Team";
 include "view-header.php";
-$rosterbyteams = selectRosterByTeam($_GET['TeamID']);
+$rosterbyteams = selectRosterByTeam($_GET['id']);
 include "view-roster-by-team.php";
 include "view-footer.php";
 ?>
