adc39277f8026d792393b9a75ae29420a95cccd7
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 13:32:04 -0500

    Update view-players-with-stats.php

--- view-players-with-stats.php
+++ view-players-with-stats.php
@@ -7,14 +7,19 @@ while ($player = $playerswithstats->fetch_assoc()){
     <div class="card-body">
       <h5 class="card-title"><?php echo $player['PlayerName']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
   $stats = selectStatsByPlayer($player['PlayerID']);
   while ($playerstat = $playerstats->fetch_assoc()) {
 ?>
-    
+    <li class="list-group-item">Games Played: <?php echo $playerstat['GamesPlayed']; ?></li>
+    <li class="list-group-item">PPG: <?php echo $playerstat['PPG']; ?></li>
+    <li class="list-group-item">APG: <?php echo $playerstat['APG']; ?></li>
+    <li class="list-group-item">RPG: <?php echo $playerstat['RPG']; ?></li>
 <?php
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Position: <?php echo $player['PlayerPosition']; ?></small></p>
     </div>
