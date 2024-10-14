d7bbc39e650418b5407b429029e4844e00f90e4a
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 12:06:21 -0500

    Create view-players.php

---
+++ view-players.php
@@ -0,0 +1,27 @@
+<h1>NBA Players - Northwest Division</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Player ID</th>
+      <th>Player Name</th>
+      <th>Position</th>
+      <th>Team ID</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($player = $nbaplayers->fetch_assoc()){
+?>
+  <tr>
+    <td><?php echo $player['PlayerID']; ?></td>
+    <td><?php echo $player['PlayerName']; ?></td>
+    <td><?php echo $player['PlayerPosition']; ?></td>
+    <td><?php echo $player['TeamID']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
