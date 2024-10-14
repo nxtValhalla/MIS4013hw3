32f3e607253a707d161546872216567c80401cd0
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 10:37:46 -0500

    Update address-by-location.php

--- address-by-location.php
+++ address-by-location.php
@@ -4,7 +4,23 @@ require_once("model-address-by-location.php");
 
 $pageTitle = "Address by Location";
 include "view-header.php";
-$addressbylocation = selectAddressByLocation($_POST['lid']);
+
+// Check if $_POST['lid'] is set and pass it to the query
+if (isset($_POST['lid'])) {
+    $lid = $_POST['lid'];
+    echo "Location ID: " . $lid; // Debugging line to verify POST data
+
+    $addressbylocation = selectAddressByLocation($_POST['lid']);
+
+    if ($addressbylocation->num_rows > 0) {
+        include "view-address-by-location.php";
+    } else {
+        echo "No results found for Location ID: " . $lid;
+    }
+} else {
+    echo "Location ID not set!";
+}
+  
 include "view-address-by-location.php";
 include "view-footer.php";
 ?>
