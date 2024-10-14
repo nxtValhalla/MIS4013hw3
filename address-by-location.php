055a4ae710b03523764e6677199a8fb1a890c208
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 02:20:25 -0500

    Create address-by-location.php

---
+++ address-by-location.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-address-by-location.php");
+
+$pageTitle = "Address by Location";
+include "view-header.php";
+$addresses = selectAddressByLocation($_POST['Locid']);
+include "view-address-by-location.php";
+include "view-footer.php";
+?>
