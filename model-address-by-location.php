5cbdad724057becd6a6ca2419f9afbc1d4f7a02f
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 10:18:22 -0500

    Update model-address-by-location.php

--- model-address-by-location.php
+++ model-address-by-location.php
@@ -2,7 +2,7 @@
 function selectAddressByLocation($Locid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT l.ArenaID, ArenaName, Address, a.City, a.State, ZipCode FROM nbarosters.nba_team_locations l join nbarosters.nba_arena_addresses a on l.ArenaID = a.ArenaID WHERE l.LocationID = 1");
+        $stmt = $conn->prepare("SELECT l.ArenaID, ArenaName, Address, a.City, a.State, ZipCode FROM nbarosters.nba_team_locations l join nbarosters.nba_arena_addresses a on l.ArenaID = a.ArenaID WHERE l.LocationID = ?");
         $stmt->bind_param("i", $Locid);
         $stmt->execute();
         $result = $stmt->get_result();
