262d1dac6b2fb5db2b27558b459fdbba721398c5
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 12:40:11 -0500

    Create view-players-with-stats.php

---
+++ view-players-with-stats.php
@@ -0,0 +1,22 @@
+<h1>NBA Players - Northwest Division</h1>
+<div class="card-group">
+<?php
+while ($player = $playerswithstats->fetch_assoc()){
+?>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $player['PlayerName']; ?></h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <tr>
+    <td><?php echo $player['PlayerID']; ?></td>
+    <td><?php echo $player['PlayerName']; ?></td>
+    <td><?php echo $player['PlayerPosition']; ?></td>
+    <td><?php echo $player['TeamID']; ?></td>
+  </tr>
+<?php
+}
+?>
+</div>
