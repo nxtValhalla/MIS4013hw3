aa29973b85f98a0614fae657ce558ed30a1bd0f8
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-13 19:47:39 -0500

    Update model-roster-by-team.php

--- model-roster-by-team.php
+++ model-roster-by-team.php
@@ -2,7 +2,7 @@
 function selectRosterByTeam($TeamID) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT d.TeamID, d.TeamName, PlayerID, PlayerName, PlayerPosition FROM nbarosters.nba_northwest_division d join nbarosters.nba_northwest_players p on d.TeamID = p.TeamID WHERE d.TeamID = ?");
+        $stmt = $conn->prepare("SELECT PlayerID, PlayerName, PlayerPosition FROM nbarosters.nba_northwest_division d join nbarosters.nba_northwest_players p on d.TeamID = p.TeamID WHERE d.TeamID = ?");
         $stmt->bind_param("i", $TeamID);
         $stmt->execute();
         $result = $stmt->get_result();
