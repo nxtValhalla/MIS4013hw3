e83c0df3d4b66dbe2a674cbfbaf215f5d2e033fd
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 11:47:20 -0500

    Update view-locations.php

--- view-locations.php
+++ view-locations.php
@@ -18,8 +18,8 @@ while ($location = $locations->fetch_assoc()){
     <td><?php echo $location['City']; ?></td>
     <td><?php echo $location['State']; ?></td>
     <td>
-      <form method="POST" action="address-by-location.php">
-        <input type="hidden" name="lid" value="<?php echo $address['LocationID']; ?>">
+      <form action="address-by-location.php" method="POST">
+        <input type="hidden" name="lid" value="<?php echo $location['LocationID']; ?>">
         <button type="submit" class="btn btn-primary">Arena Address</button>
       </form>
     </td>
