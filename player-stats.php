9231c58cae73f1b7a08cbedd86e727ed484b43df
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 21:46:47 -0500

    Update player-stats.php

--- player-stats.php
+++ player-stats.php
@@ -5,6 +5,6 @@ require_once("model-player-stats.php");
 $pageTitle = "NBA Player Stats - Northwest Division";
 include "view-header.php";
 $playerstats = selectPlayerStats();
-include "view-playerstats.php";
+include "view-player-stats.php";
 include "view-footer.php";
 ?>
