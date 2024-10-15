816f364ccdfdef4b8ed23f89522a9df99b535f70
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-14 21:41:38 -0500

    Update view-players-with-stats.php

--- view-players-with-stats.php
+++ view-players-with-stats.php
@@ -1,37 +1,40 @@
 <h1>NBA Players With Stats - Northwest Division</h1>
-<div class="card-group">
+<div class="container">
 <?php
 if ($players->num_rows > 0) {
     while ($player = $players->fetch_assoc()) {
 ?>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $player['PlayerName']; ?></h5>
-      <p class="card-text">
-<!--       <ul class="list-group">
+    <div class="row mb-4">
+      <div class="card w-100">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $player['PlayerName']; ?></h5>
+          <p class="card-text">
+<!--           <ul class="list-group">
 <?php
-      // if ($playerstats->num_rows > 0) {
-      //   while ($playerstat = $playerstats->fetch_assoc()) {
+      //     if ($playerstats->num_rows > 0) {
+      //       while ($playerstat = $playerstats->fetch_assoc()) {
 ?>
-          <li class="list-group-item">Games Played: <?php echo $playerstat['GamesPlayed']; ?></li>
-          <li class="list-group-item">PPG: <?php echo $playerstat['PPG']; ?></li>
-          <li class="list-group-item">APG: <?php echo $playerstat['APG']; ?></li>
-          <li class="list-group-item">RPG: <?php echo $playerstat['RPG']; ?></li>
+              <li class="list-group-item">Games Played: <?php echo $playerstat['GamesPlayed']; ?></li>
+              <li class="list-group-item">PPG: <?php echo $playerstat['PPG']; ?></li>
+              <li class="list-group-item">APG: <?php echo $playerstat['APG']; ?></li>
+              <li class="list-group-item">RPG: <?php echo $playerstat['RPG']; ?></li>
 <?php
-  //       }
-  // }   else {
-  //         echo "<li class='list-group-item'>No stats available for this player</li>";
-  //         break;
-  //     }
+  //           }
+  //     }   else {
+  //             echo "<li class='list-group-item'>No stats available for this player</li>";
+  //             break;
+  //         }
 ?>
-      </ul> -->
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Position: <?php echo $player['PlayerPosition']; ?></small></p>
+          </ul> -->
+          </p>
+          <p class="card-text"><small class="text-body-secondary">Position: <?php echo $player['PlayerPosition']; ?></small></p>
+        </div>
+      </div>
     </div>
-  </div>
 <?php
-    }
-} else {
+        }
+} 
+else {
     echo "No players found.";
 }
 ?>
