6205ad57b7f7059a0d5c8ebd271def6ca3cb5aeb
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-15 04:42:40 -0500

    Update stats-by-player.php

--- stats-by-player.php
+++ stats-by-player.php
@@ -4,7 +4,7 @@ require_once("model-stats-by-player.php");
 
 $pageTitle = "Stats by Player";
 include "view-header.php";
-$statsbyplayer = selectStatsByPlayer($_GET['id']);
+$statsbyplayer = selectStatsByPlayer($_GET['pid']);
 include "view-stats-by-player.php";
 include "view-footer.php";
 ?>
