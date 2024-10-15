64b9a15f25198ab64fcf069b288cb72c2e2b0731
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-15 03:23:50 -0500

    Update model-stats-by-player.php

--- model-stats-by-player.php
+++ model-stats-by-player.php
@@ -1,16 +1,1 @@
-<?php
-function selectStatsByPlayer($PlayerID) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.PlayerID, GamesPlayed, PPG, APG, RPG FROM nbarosters.nba_northwest_players p join nbarosters.nba_player_stats s on p.PlayerID = s.PlayerID WHERE p.PlayerID = ?");
-        $stmt->bind_param("i", $PlayerID);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-?>
+
