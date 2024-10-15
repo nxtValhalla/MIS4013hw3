45287dcc49fa6588c4bd11d3d75710ad2cf8b9ab
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-15 04:12:57 -0500

    Update players-with-stats.php

--- players-with-stats.php
+++ players-with-stats.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-players-with-stats.php");
 
+$pageTitle = "NBA Players With Statistics - Northwest Division";
+include "view-header.php";
+$players = selectPlayers();
+include "view-players-with-stats.php";
+include "view-footer.php";
+?>
