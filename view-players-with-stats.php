98918f364932986528cb775dd6a00f56c120d93c
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-10-15 05:12:21 -0500

    Update view-players-with-stats.php

--- view-players-with-stats.php
+++ view-players-with-stats.php
@@ -1,1 +1,34 @@
-
+<h1>NBA Players with Statistics- Northwest Division</h1>
+<div class="container">
+<?php
+while ($player = $players->fetch_assoc()){
+?>
+  <div class="row mb-4">
+    <div class="card w-100">
+      <div class="card-body">
+        <h5 class="card-title">
+          ID: <?php echo $player['PlayerID']; ?> - Name: <?php echo $player['PlayerName']; ?>
+        </h5>
+        <p class="card-text">
+        <ul class="list-group">
+<?php
+  $statvars = selectStatsByPlayer($player['PlayerID']);
+  while ($statvar = $statvars->fetch_assoc()) {
+?>     
+    <li class="list-group-item">Games Played: <?php echo $statvar['GamesPlayed']; ?></li>
+    <li class="list-group-item">PPG: <?php echo $statvar['PPG']; ?></li>
+    <li class="list-group-item">APG: <?php echo $statvar['APG']; ?></li>
+    <li class="list-group-item">RPG: <?php echo $statvar['RPG']; ?></li>
+<?php
+  }
+?>
+        </ul>
+        </p>
+        <p class="card-text"><small class="text-body-secondary">Position: <?php echo $player['PlayerPosition']; ?></small></p>
+      </div>
+    </div>
+  </div>
+<?php
+}
+?>
+</div>
