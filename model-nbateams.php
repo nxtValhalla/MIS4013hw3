9d0e8d1ce0e2869514fee737d76ea12074b48b7e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-06 19:32:13 -0600

    Update model-nbateams.php

--- model-nbateams.php
+++ model-nbateams.php
@@ -12,4 +12,17 @@ function selectNBATeams() {
         throw $e;
     }
 }
+function insertNBATeam($teamName, $wins, $losses, $locID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO nbarosters.nba_northwest_division (TeamName, Wins, Losses, LocationID) VALUES (?, ?, ?, ?)");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
