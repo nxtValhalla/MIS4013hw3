694fdcd11e07c3edba11d1bef76f48521576eeb5
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 11:58:53 -0600

    Update model-players-with-stats.php

--- model-players-with-stats.php
+++ model-players-with-stats.php
@@ -27,4 +27,72 @@ function selectStatsByPlayer($PlayerID) {
         throw $e;
     }
 }
+
+function selectPlayersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT PlayerID, PlayerName FROM nbarosters.nba_northwest_players ORDER BY PlayerName;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectStatsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT PlayerID, PlayerName FROM nbarosters.nba_northwest_players ORDER BY PlayerName;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertStatsByPlayer($playerID, $statName, $statValue) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO nbarosters.nba_northwest_player_stats (PlayerID, StatName, StatValue) VALUES (?, ?, ?)");
+        $stmt->bind_param("isd", $playerID, $statName, $statValue);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateStatsByPlayer($statName, $statValue, $playerID, $statInputID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE nbarosters.nba_northwest_player_stats set StatName = ?, StatValue = ? WHERE PlayerID = ? AND StatInputID = ?");
+        $stmt->bind_param("sdii", $statName, $statValue, $playerID, $statInputID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteStatsByPlayer($statInputID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM nbarosters.nba_northwest_player_stats WHERE StatInputID = ?");
+        $stmt->bind_param("i", $statInputID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
