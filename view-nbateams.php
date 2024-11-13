7ab9dd3aecd013bad0b6ec87989eafd619c06131
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 04:35:10 -0600

    Update view-nbateams.php

--- view-nbateams.php
+++ view-nbateams.php
@@ -19,6 +19,8 @@ include "view-nbateams-addform.php";
       <th>Losses</th>
       <th>Location ID</th>
       <th></th>
+      <th></th>
+      <th></th>
       </tr>
     </thead>
     <tbody>
@@ -31,6 +33,19 @@ while ($nbateam = $nbateams->fetch_assoc()){
     <td><?php echo $nbateam['Wins']; ?></td>
     <td><?php echo $nbateam['Losses']; ?></td>
     <td><?php echo $nbateam['LocationID']; ?></td>
+    <td></td>
+    <td>
+      <form method="post" action="">
+        <input type="hidden" name="ID" value="<?php echo $nbateam['TeamID'];?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-danger">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+      </form>
+    </td>
     <td><a href="roster-by-team.php?id=<?php echo $nbateam['TeamID']; ?>">Roster</a></td>
   </tr>
 <?php
