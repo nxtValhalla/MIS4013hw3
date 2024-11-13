d8fb5177538f288a072aa525f6dee125cf528644
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 04:08:50 -0600

    Update nbateams.php

--- nbateams.php
+++ nbateams.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertNBATeam($_POST['teamName'], $_POST['wins'], $_POST['losses'], $_POST['locID'])
+      if (insertNBATeam($_POST['teamName'], $_POST['wins'], $_POST['losses'], $_POST['locID'])) {
+        echo '<div class="alert alert-success" role="alert">NBA Team Added Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Team Added.</div>';
+      }
       break;
   }
 }
