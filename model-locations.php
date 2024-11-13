89df4188676f082dad397faa14e3626da2c6dc83
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 09:36:42 -0600

    Update model-locations.php

--- model-locations.php
+++ model-locations.php
@@ -12,4 +12,43 @@ function selectLocations() {
         throw $e;
     }
 }
+function insertLocation($city, $state, $arenaID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO nbarosters.nba_team_locations (City, State, ArenaID) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $city, $state, $arenaID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateLocation($city, $state, $arenaID, $locID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE nbarosters.nba_team_locations set City = ?, State = ?, ArenaID = ? WHERE LocationID = ?");
+        $stmt->bind_param("ssii", $city, $state, $arenaID, $locID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteLocation($locID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM nbarosters.nba_team_locations WHERE LocationID = ?");
+        $stmt->bind_param("i", $locID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
