dabb3500ae009eaefa3a1d11328e6053d4f7539b
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 01:01:16 -0600

    Update model-nbateams.php

--- model-nbateams.php
+++ model-nbateams.php
@@ -16,10 +16,36 @@ function insertNBATeam($teamName, $wins, $losses, $locID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO nbarosters.nba_northwest_division (TeamName, Wins, Losses, LocationID) VALUES (?, ?, ?, ?)");
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt->bind_param("siis", $teamName, $wins, $losses, $locID);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateNBATeam($teamName, $wins, $losses, $locID, $teamid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE nbarosters.nba_northwest_division set TeamName = ?, Wins = ?, Losses = ?, LocationID = ? WHERE TeamID = ?)");
+        $stmt->bind_param("siisi", $teamName, $wins, $losses, $locID, $teamid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteNBATeam($teamid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM nbarosters.nba_northwest_division WHERE TeamID = ?");
+        $stmt->bind_param("i", $teamid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
