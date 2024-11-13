9d895ea933c379df5e906df358960e09dd3159fd
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 09:53:10 -0600

    Update locations.php

--- locations.php
+++ locations.php
@@ -4,6 +4,33 @@ require_once("model-locations.php");
 
 $pageTitle = "NBA Northwest Division Team Locations";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertLocation($_POST['city'], $_POST['state'], $_POST['arenaID'])) {
+        echo '<div class="alert alert-success" role="alert">Location Added Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Location Added.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateLocation($_POST['city'], $_POST['state'], $_POST['arenaID'], $_POST['locID'])) {
+        echo '<div class="alert alert-success" role="alert">Location Edited Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Location Edited.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteLocation($_POST['locID'])) {
+        echo '<div class="alert alert-success" role="alert">Location Deleted Successfully!</div>';   
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error. No Location Removed.</div>';
+      }
+      break;
+  }
+}
+
 $locations = selectLocations();
 include "view-locations.php";
 include "view-footer.php";
