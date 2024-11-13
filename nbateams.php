9c43c2476075fbdbe34bca803b327b952a4ede7b
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-13 04:04:56 -0600

    Update nbateams.php

--- nbateams.php
+++ nbateams.php
@@ -4,6 +4,15 @@ require_once("model-nbateams.php");
 
 $pageTitle = "NBA Teams";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertNBATeam($_POST['teamName'], $_POST['wins'], $_POST['losses'], $_POST['locID'])
+      break;
+  }
+}
+
 $nbateams = selectNBATeams();
 include "view-nbateams.php";
 include "view-footer.php";
