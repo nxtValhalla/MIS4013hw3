0df231290cb256c1354133cd3288ff2947faf92e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-18 13:29:45 -0600

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -1,4 +1,14 @@
+<div class="row">
+  <div class="col">
 <h1>NBA Players - Northwest Division</h1>
+  </div>
+  <div class="col-auto" style="display: flex; flex-direction: row; align-items: center;">
+<?php
+include "view-players-addform.php";
+?>
+<h2>Add a New Player</h2>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
