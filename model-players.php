9cff093c56f84665aa5c82781bed9612c7e0c543
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-18 13:18:26 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -12,4 +12,43 @@ function selectPlayers() {
         throw $e;
     }
 }
+function insertPlayer($playerName, $pos, $teamID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO nbarosters.nba_northwest_players (PlayerName, PlayerPosition, TeamID) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $playerName, $pos, $teamID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updatePlayer($playerName, $pos, $teamID, $playerID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE nbarosters.nba_northwest_players set PlayerName = ?, PlayerPosition = ?, TeamID = ? WHERE PlayerID = ?");
+        $stmt->bind_param("ssii", $playerName, $pos, $teamID, $playerID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deletePlayer($playerID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM nbarosters.nba_northwest_players WHERE PlayerID = ?");
+        $stmt->bind_param("i", $playerID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
