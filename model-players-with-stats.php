59a15df2916bafead93924aae7a160cbb87a4f1b
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-19 05:06:52 -0600

    Update model-players-with-stats.php

--- model-players-with-stats.php
+++ model-players-with-stats.php
@@ -27,4 +27,44 @@ function selectStatsByPlayer($PlayerID) {
         throw $e;
     }
 }
+
+function insertPlayerStat($playerid, $statname, $statvalue) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO nbarosters.nba_northwest_player_stats (PlayerID, StatName, StatValue) VALUES (?, ?, ?)");
+        $stmt->bind_param("isd", $playerid, $statname, $statvalue);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updatePlayerStat($playerid, $statname, $statvalue, $statinputid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE nbarosters.nba_northwest_player_stats set PlayerID = ?, StatName = ?, StatValue = ?, WHERE StatInputID = ?");
+        $stmt->bind_param("isdi", $playerid, $statname, $statvalue, $statinputid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deletePlayerStat($statinputid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM nbarosters.nba_northwest_player_stats WHERE StatInputID = ?");
+        $stmt->bind_param("i", $statinputid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
