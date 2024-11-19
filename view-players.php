491320d16020680a58f24e117f46330d6db91629
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-19 04:52:36 -0600

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -1,6 +1,6 @@
 <div class="row">
   <div class="col">
-<h1>NBA Players - Northwest Division</h1>
+<h1>NBA Players</h1>
   </div>
   <div class="col-auto" style="display: flex; flex-direction: row; align-items: center;">
 <?php
