85091ddb99627475b75850a70ca05fec2756f011
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-20 18:27:19 -0600

    Update view-team-input-list.php

--- view-team-input-list.php
+++ view-team-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="teamID" name="teamID">
 <?php
 while ($teamItem = $teamList -> fetch_assoc()) {
+  $selText = "";
+  if ($selectedTeam == $teamItem['TeamID']) {
+    $selText = " selected";
+  }
 ?>
-  <option value="<?php echo $teamItem['TeamID']; ?>"><?php echo $teamItem['TeamName']; ?></option>
+  <option value="<?php echo $teamItem['TeamID']; ?>"<?$selText?>><?php echo $teamItem['TeamName']; ?></option>
 <?php
 }
 ?>
