22966c7235dd20470021764762585d1db86c49a9
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-22 04:23:37 -0600

    Create js.php

---
+++ js.php
@@ -0,0 +1,25 @@
+<?php
+  include "view-header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>MIS 4013: Homework 5</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+
+    <!-- Animation -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+  </head>
+  <body>
+    
+  </body>
+</html>
+
+<?php
+  include "view-footer.php";
+?>
