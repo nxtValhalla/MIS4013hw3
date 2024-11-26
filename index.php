60672a539b682721d6ebf8948eeebfd5ba3ded26
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-26 02:29:20 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php"
 ?>
-    <h1>NBA Finals Countdown</h1>
+    <h1>NBA Finals Countdown until June 5th, 2025 at 7:30pm CST</h1>
 <?php
 include "view-index.php";
 include "view-footer.php";
