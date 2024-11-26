2be61230c54fc4cc557a39c919279a76769aa17d
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-26 02:22:12 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -4,23 +4,23 @@ include "bootstrap.php";
 include "js.php";
 ?>
 <div class="countdown-container">
-  <div class="countdown-title">Countdown to NBA Finals</div>
-  <div id="countdown">
-    <div class="time-box">
-      <h1 id="days">00</h1>
-      <span>Days</span>
-    </div>
-    <div class="time-box">
-      <h1 id="hours">00</h1>
-      <span>Hours</span>
-    </div>
-    <div class="time-box">
-      <h1 id="minutes">00</h1>
-      <span>Minutes</span>
-    </div>
-    <div class="time-box">
-      <h1 id="seconds">00</h1>
-      <span>Seconds</span>
-    </div>
-  </div>
+  <div class="countdown-title mb-4">Countdown to NBA Finals</div>
+  <table class="table table-dark table-bordered table-striped text-center">
+    <thead>
+      <tr>
+        <th>Days</th>
+        <th>Hours</th>
+        <th>Minutes</th>
+        <th>Seconds</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td id="days">00</td>
+        <td id="hours">00</td>
+        <td id="minutes">00</td>
+        <td id="seconds">00</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
