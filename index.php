72fd1728902b74727fe7543e24b5173dbf18781a
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-26 02:09:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 $pageTitle = "Home";
-include "view-header.php";
+include "view-header.php"
+?>
     <h1>NBA Finals Countdown</h1>
+<?php
 include "view-index.php";
 include "view-footer.php";
 ?>
