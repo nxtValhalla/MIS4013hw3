71d6d5e4e067c8b92cc2ef1e6b43571421ea2b0e
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-11-26 01:49:48 -0600

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,5 @@
-<!-- bootstrap-icons.php -->
+<!-- bootstrap-icons -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- bootstrap-css -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
