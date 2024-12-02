d034de9ae0a8cd43c66ca7eaec323841254fbb00
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-02 04:49:18 -0600

    Create view-index.php

---
+++ view-index.php
@@ -0,0 +1,23 @@
+<div class="countdown-container">
+  <table class="table table-dark table-bordered table-striped text-center">
+    <thead>
+      <tr>
+        <th>Days</th>
+        <th>Hours</th>
+        <th>Minutes</th>
+        <th>Seconds</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td id="days">00</td>
+        <td id="hours">00</td>
+        <td id="minutes">00</td>
+        <td id="seconds">00</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<?php
+include "js-finals-countdown.php";
+?>
