b293c3cbbf6ab9ab16e81963f55b2d8bee89121a
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-02 06:53:50 -0600

    Update view-index.php

--- view-index.php
+++ view-index.php
@@ -20,7 +20,7 @@
     </table>
   </div>
   <div class="container text-center mt-4">
-      <h1>Image Manipulation with Buttons!</h1>
+      <h1 class="nba-countdown-header">Image Manipulation with Buttons!</h1>
       <div class="btn-group mt-3" role="group">
         <button id="addSize" class="btn btn-primary"><i class="bi bi-plus-square"></i></button>
         <button id="minusSize" class="btn btn-danger"><i class="bi bi-dash-square"></i></button>
