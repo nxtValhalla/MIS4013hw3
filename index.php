8d8f04cc88ded012a9a34e1c6c990a988d2ce284
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-02 05:15:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php"
 ?>
+    <link rel="stylesheet" href="finals-countdown.css">
     <h1 class="nba-countdown-header">
       NBA Finals Countdown <br />
       <small>June 5th, 2025 at 7:30 PM CST</small>
