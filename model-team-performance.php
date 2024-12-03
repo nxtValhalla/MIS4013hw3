c8c9af33001ba05cdb1381302ae34bb85d059245
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-03 03:09:49 -0600

    Update model-team-performance.php

--- model-team-performance.php
+++ model-team-performance.php
@@ -2,7 +2,7 @@
 function selectTeamPerformanceData() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT TeamID, TeamName, Wins, Losses FROM nbarosters.nba_northwest_division;");
+        $stmt = $conn->prepare("SELECT TeamName, Wins, Losses FROM nbarosters.nba_northwest_division ORDER BY Wins DESC;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
