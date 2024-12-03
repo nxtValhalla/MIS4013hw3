e05c338c9d0d11c51aa74a294bf64423e53b7fd4
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-03 00:57:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include "view-header.php"
 ?>
     <link rel="stylesheet" href="finals-countdown.css">
 <?php
-include "view-index.php";
 include "js-finals-countdown.php";
 include "js-image-manipulation.php";
+include "view-index.php";
 include "view-footer.php";
 ?>
