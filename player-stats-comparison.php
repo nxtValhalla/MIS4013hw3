c5b4397a43c719deb757249dcb5c2c295d4423d7
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-03 03:45:30 -0600

    Update player-stats-comparison.php

--- player-stats-comparison.php
+++ player-stats-comparison.php
@@ -5,7 +5,15 @@ require_once("model-player-stats-comparison.php");
 $pageTitle = "Player Stats Comparison";
 include "view-header.php";
 
+// Stat Data
 $playerStatsData = getPlayerStatsComparison();
+
+// Array for Player Dropdowns
+$players = [];
+while ($row = $playerStatsData->fetch_assoc()) {
+    $players[] = $row;
+}
+
 include "view-player-stats-comparison.php";
 include "view-footer.php";
 ?>
