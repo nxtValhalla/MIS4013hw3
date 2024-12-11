8105569bd8965936fcde85a9734f35abbbba3836
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-11 16:42:05 -0600

    Update model-top-players-treemap.php

--- model-top-players-treemap.php
+++ model-top-players-treemap.php
@@ -8,7 +8,7 @@ function getTopPlayersByStat($statName, $limit = 10) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("
-            SELECT PlayerName, StatValue
+            SELECT PlayerName, StatValue, StatName
             FROM nbarosters.nba_northwest_player_stats s
             JOIN nbarosters.nba_northwest_players p ON s.PlayerID = p.PlayerID
             WHERE s.StatName = ?
