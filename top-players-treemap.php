db3a655cf8d70edf90e8491d8a41cf898c462949
Nathaniel Pumphrey <143319248+nxtValhalla@users.noreply.github.com>
2024-12-11 16:49:09 -0600

    Update top-players-treemap.php

--- top-players-treemap.php
+++ top-players-treemap.php
@@ -2,20 +2,23 @@
 require_once("util-db.php");
 require_once("model-top-players-treemap.php");
 
-$pageTitle = "Top Players by Stat";
+$pageTitle = "Player Stats Treemap";
 include "view-header.php";
 
+// Default stat to display
 $defaultStat = "PPG";
+$statName = isset($_GET['stat']) ? $_GET['stat'] : $defaultStat;
 
-$statName = isset($_GET['stat']) && in_array($_GET['stat'], ['PPG', 'RPG', 'APG']) ? $_GET['stat'] : $defaultStat;
+// Fetch data for the specified stat
+$playerStats = getPlayerStatsForTreemap($statName);
 
-$playerData = getTopPlayersByStat($statName);
-
-$players = [];
-while ($row = $playerData->fetch_assoc()) {
-    $players[] = $row;
+// Convert data to an array for Chart.js
+$data = [];
+while ($row = $playerStats->fetch_assoc()) {
+    $data[] = $row;
 }
 
+// Available stats for selection
 $availableStats = ['PPG', 'RPG', 'APG'];
 
 include "view-top-players-treemap.php";
